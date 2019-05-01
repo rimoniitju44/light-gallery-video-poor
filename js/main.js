ffe3cc96dc740bf357256edff05843906be7810e
rimoniitju44 <rimonmon10@gmail.com>
2019-05-02 01:07:47 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('#video-gallery').lightGallery({
+        youtubePlayerParams: {
+            modestbranding: 1,
+            showinfo: 0,
+            rel: 0,
+            controls: 0
+        }
+
+    }); 
+
+});
